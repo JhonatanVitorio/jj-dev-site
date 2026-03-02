6cdb1688404f373defb4abd2b86b1344928b45b3
Jhonatan Vitorio <jhonatan06silveira@gmail.com>
2026-03-02 19:15:02 -0300

    add IDs emailjs

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -10,9 +10,9 @@ type FormState = {
 }
 
 export function Contact() {
-  const serviceId = import.meta.env.VITE_EMAILJS_SERVICE_ID as string
-  const templateId = import.meta.env.VITE_EMAILJS_TEMPLATE_ID as string
-  const publicKey = import.meta.env.VITE_EMAILJS_PUBLIC_KEY as string
+  const serviceId = "service_vhhyd3b"
+  const templateId = "template_3js1324"
+  const publicKey = "V6ODApXPvukiLwIpt"
 
   const [form, setForm] = useState<FormState>({
     name: "",
